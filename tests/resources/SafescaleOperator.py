6cbea3ab68abf3b09a8add1baca7c30b0dd88dd5
William RUFFINE <william.ruffine@csgroup.eu>
2022-04-05 11:52:40 +0200

    doc: adding docstring to library

--- tests/resources/SafescaleOperator.py
+++ tests/resources/SafescaleOperator.py
@@ -7,6 +7,13 @@ from RobotVault import RobotVault
 class SafescaleOperator:
 
     def get_flex_credentials():
+        """_summary_
+        Retrieves the safescale tenants file and use it to
+        retrieve the flexible engine credentials
+
+        Returns:
+            tuple: the flexible engine credentials
+        """
         RobotVault.get_tenant_file()
         with open("tenants.yaml",'r') as f:
             docs = yaml.safe_load(f)
@@ -21,6 +28,13 @@ class SafescaleOperator:
         return(username, password, domainName, projectId)
     
     def get_flex_token():
+        """_summary_
+        Uses the flexible engine credentials to retrieve
+        the flexible engine token via the API
+
+        Returns:
+            string: the flexible engine token
+        """
         CRED = SafescaleOperator.get_flex_credentials()
         url = "https://iam.eu-west-0.prod-cloud-ocb.orange-business.com/v3/auth/tokens"
         header = {"Content-Type": "application/json"}
@@ -48,13 +62,24 @@ class SafescaleOperator:
         return token.headers['X-Subject-Token']
     
     def get_gateway_ip(self):
+        """_summary_
+        Uses the flexible engine api to retrieve the cluster gateway ip
+
+        Returns:
+            string: the gateway ip
+        """
+
         TOKEN = SafescaleOperator.get_flex_token()
         CRED = SafescaleOperator.get_flex_credentials()
+        
         URL = "https://ecs.eu-west-0.prod-cloud-ocb.orange-business.com/v2.1/"+CRED[3]+"/servers"
         HEADER = {"Content-Type": "application/json", "X-Auth-Token": TOKEN}
+        # Get the gateway IPs
         servers = requests.get(url=URL, headers=HEADER)
         gatewayId = list(filter((lambda c: c['name'] == "gw-"+str(os.environ["CLUSTER_NAME"])),(servers.json()["servers"])))[0]['id']
+        # Get the gateway description to retrieve its ip
         URL2 = URL+"/"+str(gatewayId)
         gatewayDetails = requests.get(url=URL2, headers=HEADER)
         addrKey = next(iter(gatewayDetails.json()["server"]["addresses"]))
+
         return(gatewayDetails.json()["server"]["addresses"][addrKey][1]['addr'])
